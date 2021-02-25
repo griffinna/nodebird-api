df43c9364ce381e7cf9733705af8fa3a2ee4d9e1
Griffin <garam.dev9@gmail.com>
2021-02-25 21:31:47 +0900

    Add: 도메인 모델을 시퀄라이즈와 연결

--- models/index.js
+++ models/index.js
@@ -4,6 +4,7 @@ const config = require('../config/config')[env];
 const User = require('./user');
 const Post = require('./post');
 const Hashtag = require('./hashtag');
+const Domain = require('./domain');
 
 const db = {};
 const sequelize = new Sequelize(
@@ -14,13 +15,16 @@ db.sequelize = sequelize;
 db.User = User;
 db.Post = Post;
 db.Hashtag = Hashtag;
+db.Domain = Domain;
 
 User.init(sequelize);
 Post.init(sequelize);
 Hashtag.init(sequelize);
+Domain.init(sequelize);
 
 User.associate(db);
 Post.associate(db);
 Hashtag.associate(db);
+Domain.associate(db);
 
 module.exports = db;
