14824a19edbe9f0b0c7e0af667021281042b1c08
Griffin <garam.dev9@gmail.com>
2021-02-26 21:03:11 +0900

    Add: 토큰 인증 미들웨어 추가

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -1,4 +1,5 @@
 // 라우터에 접근 권한을 제어하는 미들웨어
+const jwt = require('jsonwebtoken');
 
 exports.isLoggedIn = (req, res, next) => {
     // Passport 는 req 객체에 isAuthenticated 메서드를 추가함 (로그인중이라면 true, 아니면 false)
@@ -15,5 +16,23 @@ exports.isNotLoggedIn = (req, res, next) => {
     } else {
         const message = encodeURIComponent('로그인한 상태입니다.');
         res.redirect(`/?error=${message}`);
+    }
+};
+
+exports.verifyToken = (req, res, next) => {
+    try {
+        req.decode = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+        return next();
+    } catch (error) {
+        if(error.name === 'TokenExpiredError') {    // 유효기간 초과
+            return res.status(419).json({
+                code: 419,
+                message: '토큰이 만료되었습니다.',
+            });
+        }
+        return res.status(401).json({
+            code: 401,
+            message: '유효하지 않은 토큰입니다.',
+        });
     }
 };
