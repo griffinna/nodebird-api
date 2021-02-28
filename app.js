cccb1f6b491a16b4b623eec7af11dd876771fe4b
Griffin <garam.dev9@gmail.com>
2021-02-28 18:08:33 +0900

    Add: v2 라우터 app 연결

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
 
 dotenv.config();
-const v1 = require('./routes/v1');
+const v1 = require('./routes/v1'); 
+const v2 = require('./routes/v2'); 
 const authRouter = require('./routes/auth');
 const indexRouter = require('./routes');
 const { sequelize } = require('./models');
@@ -48,6 +49,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/v1', v1);
+app.use('/v2', v2);
 app.use('/auth', authRouter);
 app.use('/', indexRouter);
 
